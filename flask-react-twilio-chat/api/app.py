553b46d8769e94cdf85521e8e988d78b1538aa7c
Akshay <kshirsagar1408@gmail.com>
2021-02-27 16:39:47 -0500

    Added authentication functionality.

--- flask-react-twilio-chat/api/app.py
+++ flask-react-twilio-chat/api/app.py
@@ -46,3 +46,45 @@ def delete(name):
 		print("Chat room not found")
 	else:
 		conversation.delete()
+
+
+@app.route('/login',methods =['POST'])
+def login():
+	payload = request.get_json(force=True)
+	username = payload.get('username')
+	if not username:
+		abort(401)
+
+	# create user if it does not exist
+	participant_role_sid = None
+	for role in twilio_client.conversations.roles.list():
+		if role.friendly_name == 'participant':
+			participant_role_sid = role.sid
+	try:
+		twilio_client.conversations.users.create(identify=username, role_sid=participant_role_sid)
+	except TwilioRestException as exc:
+		if exc.status!=409:
+			raise
+
+	# add the user to all conversations
+	conversations = twilio_client.conversations.conversations.list()
+	for conversation in conversations:
+		try:
+			conversation.participants.create(identify=username)
+		except TwilioRestException as exc:
+			if exc!=409:
+				raise
+
+	# generate an access token
+	twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+	twilio_api_key_sid = os.environ.get('TWILIO_API_KEY_SID')
+	twilio_api_key_secret = os.environ.get('TWILIO_API_KEY_SECRET')
+	service_sid  = conversations[0].chat_service_sid
+	token = AccessToken(twilio_account_sid, twilio_api_key_sid, twilio_api_key_secret, identify=username)
+	token.add_grant(ChatGrant(service_sid = service_sid))
+
+	#send a response
+	return {
+	'chatrooms': [[conversation.friendly_name, conversation.sid] for conversation in conversations],
+	'token': token.to_jwt().decode(),
+	}
